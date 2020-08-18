00e39b2c48a999cab96eb594a0ba5a4c9511e556
SudhaNarayanan <ahdusnarayanan@gmail.com>
2020-08-18 07:00:09 +0530

    Updates

---
+++ precache-manifest.641a8fc4331ad0a987cfc44956dd6e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a6eed94d7100ccb43f70dbdd25edf9",
+    "url": "/GaneshaPuja/index.html"
+  },
+  {
+    "revision": "d460551efc7e4352d336",
+    "url": "/GaneshaPuja/static/js/2.e07c9855.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GaneshaPuja/static/js/2.e07c9855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144ff0a0356f0640372b",
+    "url": "/GaneshaPuja/static/js/main.3c66e84b.chunk.js"
+  },
+  {
+    "revision": "a100952bd05758ae9843",
+    "url": "/GaneshaPuja/static/js/runtime-main.cc7d33ab.js"
+  },
+  {
+    "revision": "5f841b5dd1ea0985a2ef5de5597ab415",
+    "url": "/GaneshaPuja/static/media/GaneshJi.5f841b5d.jpg"
+  }
+]);
